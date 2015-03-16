90fcf6d2bdbdc39ae362d6897cc0d3a0ccd8f6ff
Steven Spriggs <steven.spriggs@gmail.com>
2015-03-16 16:11:02 -0400

    change upgrade message to coincide with the Hammer VM upgrade process.

--- hammer/hammer_server.rb
+++ hammer/hammer_server.rb
@@ -71,7 +71,7 @@ g = Git.open("../")
 ref = g.log.first {|l| l.sha }
 remote = g.lib.send(:command, 'ls-remote').split(/\n/)[1].split(/\t/)[0]
 
-if ref.to_s != remote.to_s
+if ref.to_s == remote.to_s
   update_url = "https://github.com/wvuweb/hammer/wiki/Update"
   puts " "
   puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
@@ -80,25 +80,16 @@ if ref.to_s != remote.to_s
   puts " "
   puts "Repository is currently at ref: ".colorize(:light_white)+(ref.to_s+" ").colorize(:light_magenta)
   puts "Remote is currently at ref: ".colorize(:light_white)+(remote.to_s+" ").colorize(:light_magenta)
-  puts "Learn how to update Hammer at: ".colorize(:light_white)+update_url.colorize(:light_cyan)
+  # puts "Learn how to update Hammer at: ".colorize(:light_white)+update_url.colorize(:light_cyan)
   puts " "
-  puts "Do you want to continue without upgrading?".colorize(:light_white)+" (Y/n) ?".colorize(:light_green)
-  if Gem.win_platform?
-    input = STDIN.gets.chomp
-  else
-    input = gets.chomp
-  end
-  if input == 'n' then
-    if os == :macosx
-      system("open "+update_url)
-    elsif os == :windows
-      system("start "+update_url)
-    elsif os == :linux || :unix
-      system("xdg-open "+update_url)
-    end
-    puts "Opening: ".colorize(:light_white)+update_url.colorize(:light_cyan)+" for updating instructions."
-    exit
-  end
+  puts " "
+  puts "Update Hammer by using using the following command: ".colorize(:light_white)
+  puts " "
+  puts "vagrant hammer update".colorize(:light_green)
+  puts " "
+  puts "Hammer will automatically restart after updating itself".colorize(:light_white)
+  puts " "
+  puts " "
 end
 
 doc_root = options.directory
