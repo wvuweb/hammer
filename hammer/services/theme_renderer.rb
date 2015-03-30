374d5721ef446e102930d21ac856ffedbdafb076
Steven Spriggs <steven.spriggs@gmail.com>
2015-03-30 16:33:26 -0400

    Add ability to include page properties javascript in `mock_data.yml` and have hammer inject it on the page.
    Syntax is as follows, hammer will add a script tag no need to include that in your `mock_data.yml`
    
    ```YAML
    page:
      javascript: |
        // create an alert dialog
        alert('hello')
    ```
    
    Closes #27

--- hammer/services/theme_renderer.rb
+++ hammer/services/theme_renderer.rb
@@ -8,10 +8,10 @@ require "../hammer/services/mock_data.rb"
 require "../hammer/services/theme_context.rb"
 
 class ThemeRenderer
-  
+
   attr_accessor :config, :server, :request, :document_root, :filesystem_path, :request_root, :theme_root, :output
   attr_accessor :content, :layout_file_path, :data
-  
+
   def initialize(options)
     @server = options[:server]
     @request = options[:request]
@@ -23,10 +23,10 @@ class ThemeRenderer
     @content = file_contents
     @output = ''
   end
-  
+
   def theme_root
     config_path = Pathname.new('config.yml')
-    
+
     @filesystem_path.ascend { |parent|
       if parent.directory?
         if parent.join(config_path).exist?
@@ -36,19 +36,19 @@ class ThemeRenderer
       end
     }
   end
-  
+
   def load_data
     MockData.load(@theme_root)
   end
-  
+
   def file_contents
     @filesystem_path.read
   end
-  
+
   def has_layout?
     self.config['layout']
   end
-  
+
   def layout_file_path
     file = self.config['layout']+'.html'
     folder = 'views/layouts'
@@ -59,19 +59,19 @@ class ThemeRenderer
     else
       raise 'Config File does not exist in theme see example: https://github.com/wvuweb/cleanslate-toolkit/blob/master/config.yml'
     end
-    begin 
+    begin
       layout_file_path.exist?
       layout_file_path
     rescue => e
       raise 'Layout File does not exist '+layout_file_path.to_s
     end
-      
+
   end
-  
+
   def layout_content
     @layout_content ||= layout_file_path.read
   end
-  
+
   def render
     parse_yaml(@content)
     unless @content
@@ -80,11 +80,11 @@ class ThemeRenderer
     render_with_radius
 
   end
-  
+
   def radius_parser(context = {})
     @radius_parser ||= Radius::Parser.new(context, :tag_prefix => 'r')
   end
-  
+
   protected
   def parse_yaml(content)
     regex = /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
@@ -99,23 +99,23 @@ class ThemeRenderer
     end
     self.config ||= {}
   end
-  
+
   def render_with_radius
     context = ThemeContext.new(self)
     parsed_content = radius_parser(context).parse(@content)
     radius_parser.context.globals.layout = false
-    
+
     if has_layout?
-      
+
       radius_parser.context.globals.yield = parsed_content
       radius_parser.context.globals.layout = true
-      
+
       radius_parser.context.globals.layout_file_path = layout_file_path
-      
+
       layout_content
-      
+
       output = radius_parser.parse(self.layout_content)
-      
+
       if self.data && self.data['livereload']
         @htmldoc = Nokogiri::HTML::Document.parse(output)
         script = Nokogiri::XML::Node.new "script", @htmldoc
@@ -124,24 +124,34 @@ class ThemeRenderer
         @htmldoc.at('head').add_child(script)
         output = @htmldoc.to_html
       end
-      
+
       if self.data && self.data['browsersync']
         @htmldoc = Nokogiri::HTML::Document.parse(output)
         script = Nokogiri::XML::Node.new "script", @htmldoc
-        
+
         script.content = self.data['browsersync-data']
-        
+
         @htmldoc.at('body').add_child(script)
         output = @htmldoc.to_html
       end
-      
-      
+
+      if self.data && self.data['page'] && self.data['page']['javascript']
+
+        @htmldoc = Nokogiri::HTML::Document.parse(output)
+        script = Nokogiri::XML::Node.new "script", @htmldoc
+
+        script.content = self.data['page']['javascript']
+
+        @htmldoc.at('body').add_child(script)
+        output = @htmldoc.to_html
+      end
+
       self.output << output
-      
+
     else
       self.output << parsed_content
     end
-    
+
   end
-  
+
 end
