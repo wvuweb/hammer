d87df7582f5fa22ea33c9110f2e44efa68827698
Steven Spriggs <steven.spriggs@gmail.com>
2015-07-17 10:00:14 -0400

    all patrolling of mock_data.yml into a folder called data at the root level.

--- hammer/services/mock_data.rb
+++ hammer/services/mock_data.rb
@@ -2,11 +2,38 @@
 require 'erb'
 require 'active_support/all'
 
+module YAML
+  @@file_root = ''
+  def self.set_file_root(x)
+    @@file_root = x
+  end
+
+  def YAML.include file_name
+    require 'erb'
+    yml_path =  @@file_root.join(Pathname.new('data/'+file_name))
+
+    if yml_path.exist?
+      erb = ERB.new(yml_path.read, nil, '-')
+      erb.result(binding)
+    else
+      puts ' '
+      puts 'WARNING:'.red
+      puts '!'.black.on_red * (44 + file_name.length)
+      puts '!    Your theme does not include a '.black.on_red+file_name.black.on_red+' file   !'.black.on_red
+      puts '!'.black.on_red * (44 + file_name.length)
+      puts ' '
+    end
+  end
+end
+
 
 class MockData
 
   def self.load(theme_root, request_path)
     @request_path = request_path
+
+    YAML.set_file_root(theme_root)
+
     if theme_root
       yml_path = theme_root.join(Pathname.new('mock_data.yml'))
       if yml_path.exist?
@@ -41,5 +68,6 @@ class MockData
       puts ' '
     end
   end
+
 
 end
