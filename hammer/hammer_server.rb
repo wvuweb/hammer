82507706b5440401831d929e171587994c819041
Steven Spriggs <steven.spriggs@gmail.com>
2016-02-29 13:16:24 -0500

    catch Git.open failures and rescue with appropriate error message.  Closes #32

--- hammer/hammer_server.rb
+++ hammer/hammer_server.rb
@@ -68,47 +68,59 @@ end
 
 
 g = Git.open("../")
-ref = g.log.first {|l| l.sha }
-remote = g.lib.send(:command, 'ls-remote').split(/\n/)[1].split(/\t/)[0]
+begin
+  ref = g.log.first {|l| l.sha }
+  remote = g.lib.send(:command, 'ls-remote').split(/\n/)[1].split(/\t/)[0]
 
-if ref.to_s != remote.to_s
-  update_url = "https://github.com/wvuweb/hammer/wiki/Update"
-  puts " "
-  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
-  puts "!!!".colorize(:red)+" WARNING YOU ARE BEHIND ON HAMMER VERSIONS".colorize(:light_cyan)+" !!!".colorize(:red)
-  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
-  puts " "
-  puts "Repository is currently at ref: ".colorize(:light_white)+(ref.to_s+" ").colorize(:light_magenta)
-  puts "Remote is currently at ref: ".colorize(:light_white)+(remote.to_s+" ").colorize(:light_magenta)
-  # puts "Learn how to update Hammer at: ".colorize(:light_white)+update_url.colorize(:light_cyan)
-  puts " "
-  puts " "
-  puts "Update Hammer by using using the following command: ".colorize(:light_white)
-  puts " "
-  puts "vagrant hammer update".colorize(:light_green)
-  puts " "
-  puts "Hammer will automatically restart after updating itself".colorize(:light_white)
-  puts " "
-  puts " "
+  if ref.to_s != remote.to_s
+    update_url = "https://github.com/wvuweb/hammer/wiki/Update"
+    puts " "
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
+    puts "!!!".colorize(:red)+" WARNING YOU ARE BEHIND ON HAMMER VERSIONS".colorize(:light_cyan)+" !!!".colorize(:red)
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
+    puts " "
+    puts "Repository is currently at ref: ".colorize(:light_white)+(ref.to_s+" ").colorize(:light_magenta)
+    puts "Remote is currently at ref: ".colorize(:light_white)+(remote.to_s+" ").colorize(:light_magenta)
+    # puts "Learn how to update Hammer at: ".colorize(:light_white)+update_url.colorize(:light_cyan)
+    puts " "
+    puts " "
+    puts "Update Hammer by using using the following command: ".colorize(:light_white)
+    puts " "
+    puts "vagrant hammer update".colorize(:light_green)
+    puts " "
+    puts "Hammer will automatically restart after updating itself".colorize(:light_white)
+    puts " "
+    puts " "
+  end
+rescue
+  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
+  puts "!!!".colorize(:red)+" COULD NOT CHECK HAMMER REPOSITORY FOR UPDATES".colorize(:light_cyan)+" !!!".colorize(:red)
+  puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
 end
 
- doc_root = options.directory
 
+doc_root = options.directory
 if File.directory?(doc_root+"/code")
 
   code = Git.open(doc_root+"/code")
-  code_ref = code.log.first {|l| l.sha}
-  code_remote = code.lib.send(:command,'ls-remote').split(/\n/)[1].split(/\t/)[0]
+  begin
+    code_ref = code.log.first {|l| l.sha}
+    code_remote = code.lib.send(:command,'ls-remote').split(/\n/)[1].split(/\t/)[0]
 
-  if code_ref.to_s != code_remote.to_s
-    puts " "
-    puts "WARNING:".colorize(:red)
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:light_cyan)
-    puts "Your ".colorize(:red)+"Code".colorize(:light_white)+" Repository is out of date, please update it ".colorize(:red)
-    puts "by changing directory into ".colorize(:red)+"'/cleanslate_themes/code'".colorize(:light_green)
-    puts "then running ".colorize(:red)+"git pull".colorize(:light_green)
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:light_cyan)
-    puts " "
+    if code_ref.to_s != code_remote.to_s
+      puts " "
+      puts "WARNING:".colorize(:red)
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:light_cyan)
+      puts "Your ".colorize(:red)+"Code".colorize(:light_white)+" Repository is out of date, please update it ".colorize(:red)
+      puts "by changing directory into ".colorize(:red)+"'/cleanslate_themes/code'".colorize(:light_green)
+      puts "then running ".colorize(:red)+"git pull".colorize(:light_green)
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:light_cyan)
+      puts " "
+    end
+  rescue
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
+    puts "!!!".colorize(:red)+" COULD NOT CHECK CODE REPOSITORY FOR UPDATES".colorize(:light_cyan)+" !!!".colorize(:red)
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".colorize(:red)
   end
 
 else
