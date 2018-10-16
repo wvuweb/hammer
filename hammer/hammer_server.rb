1d361da95e6c1811893115949392d5b4c249dc91
Steven Spriggs <>
2018-10-16 16:07:53 -0400

    add output for identity files

--- hammer/hammer_server.rb
+++ hammer/hammer_server.rb
@@ -117,9 +117,11 @@ doc_root = options.directory
 if File.directory?(doc_root+"/code")
 
   # code = Git.open(doc_root+"/code")
-  
+
   # Add identity files for bitbucket
   File.chmod(0600,"./config/hammer")
+
+  puts "Adding Identity files to be able to access Code repository in bitbucket"
   code_ref_cmd = " ssh-agent bash -c 'ssh-add ./config/hammer; cd #{doc_root+'/code'} && git rev-parse master'"
   code_remote_cmd = "ssh-agent bash -c 'ssh-add ./config/hammer; cd #{doc_root+'/code'} && git ls-remote'"
 
